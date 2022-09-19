b38239aa5e4e055d27397acc3df86b1a09b384d1
RamS24 <AlSaadi.Rami24@gmail.com>
2022-09-19 19:50:37 +0200

    sol

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,48 @@
+log = console.log;
+console.clear();
+//1.
+log('<', wrapper.tagName, '>children: ', wrapper.childElementCount);
+for (const child of wrapper.children) {
+    log(' - <', child.tagName, '>children: ', child.children.length);
+    for (const grandChild of child.children) {
+        console.log('         - <', grandChild.nodeName, '>children: ', grandChild.children.length);
+        for (const ggrandChild of grandChild.children) {
+            console.log('            - <', ggrandChild.nodeName, '>children: ', ggrandChild.children.length);
+            for (const gggrandChild of ggrandChild.children) {
+                console.log('               - <', gggrandChild.nodeName, '>children: ', gggrandChild.children.length);
+                for (const ggggrandChild of gggrandChild.children) {
+                    console.log('                    - <', ggggrandChild.nodeName, '>children: ', ggggrandChild.children.length);
+                    for (const gggggrandChild of ggggrandChild.children) {
+                        console.log('                    - <', gggggrandChild.nodeName, '>children: ', gggggrandChild.children.length);
+                        for (const ggggggrandChild of gggggrandChild.children) {
+                            console.log('                          - <', ggggggrandChild.nodeName, '>children: ', ggggggrandChild.children.length);
+                            for (const gggggggrandChild of ggggggrandChild.children) {
+                                console.log('                                  - <', gggggggrandChild.nodeName, '>children: ', gggggggrandChild.children.length);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// let children = wrapper.children;
+// let child = children[0];
+// while (child && children) {
+//     for (child of children) {
+//         console.log('list: ', [...child.children], 'number: ', child.children.length);
+//         //        child.style.backgroundColor = 'blue';
+//     }
+//     children = child.children;
+//     child = children[0];
+// }
+
+
 
 // 2.
-
-log = console.log;
 // console.log(wrapper);
 // console.log(wrapper.children);
-
-
 function maxClass() {
     let result = Array.from(wrapper.children).reduce((acc, curr) => {
         //        log('new loop', acc); log(acc?.classList?.length); log(curr.classList.length); log(curr);
@@ -13,5 +50,4 @@ function maxClass() {
     }, []);
     return { 'elementMaxClasses': result, 'maxClassesNumber': result.classList.length };
 }
-
-console.log("Object { element with the most classes, number of classes}", maxClass());
+// console.log("Object { element with the most classes, number of classes}", maxClass());
